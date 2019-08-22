0947792d4e50111cd88af6607f2b0b557dfe5e2b
souleymane <souleymanedialloo@yahoo.fr>
2019-08-22 21:39:14 +0200

    add user register

--- pyblog/settings.py
+++ pyblog/settings.py
@@ -125,4 +125,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR
 
-CRISPY_TEMPLATE_PACK = 'Bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- pyblog/urls.py
+++ pyblog/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog.urls", namespace="blog")),
+    path('register/', user_views.register, name="register"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
