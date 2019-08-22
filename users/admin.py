7517f935da009d366f2bf32597de263655ccfe22
souleymane <souleymanedialloo@yahoo.fr>
2019-08-23 01:40:08 +0200

    profil V1

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profil
 
 # Register your models here.
+
+@admin.register(Profil)
+class ProfilAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user')
+    list_display_links = ('user',)
+
+    list_filter = ('user',)
+    search_fields = ['user']
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="profil_pics", default="profil.png")
+    information = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.username} Profil'
